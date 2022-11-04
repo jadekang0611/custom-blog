04fd620a0dd1d83de81c100366dbc968cb5d1ec3
jadekang0611 <dandyloper@Jades-iMac.local>
2022-11-03 18:46:45 -0700

    Built the featuredPost query, styled the featuredPostCard component and FeaturedPost section as well as Carousel

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Head from "next/head";
+import FeaturedPosts from "../sections/FeaturedPosts";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services/index.js";
 
 const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Custom Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
